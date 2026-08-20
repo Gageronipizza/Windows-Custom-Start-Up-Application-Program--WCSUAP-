474ca4ca9b468cff59b10e4125b9240d039e8023
Gage Thomas <okidokipokiwokie@gmail.com>
2026-08-20 21:30:05 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This script was made for python 3.12, probably works for 3.13, idk im not smart enough to know
+
+The items inside of files.json are just dummy files, they proabably wont open anything
+Replace them with your actual file path's
+Only tested with .exe files
+Other files may or may not open
+No Linux support (that I know of)
+Coded myself
---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+import json
+import subprocess
+
+
+
+files = []
+
+
+with open("files.json", "r") as file:
+    files = json.load(file)
+
+print(f"{files}")
+print(f"{files[0]}")
+
+
+
+
+
+
+for i in range(files.length):
+
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    startupinfo.wShowWindow = 7
+    subprocess.Popen([r"file[i]"], startupinfo=startupinfo)
+
+
+    time.sleep(0.15)
